5e15c48e3764682013bc0b09845b5982e4f1637e
Frank Harris <fharris@etsy.com>
2013-05-11 15:40:27 -0400

    Added missing files

---
+++ ads/dartconfig.js
@@ -0,0 +1,38 @@
+{
+  "details"       : {
+    "siteName"              : "details",
+    "dartRefresh"           : false,
+    "dartRefreshTimeMsec"   : 0,
+    "slideShowAdsRefresh"   : true
+  },
+  "self"          : {
+    "siteName"              : "self",
+    "dartRefresh"           : false,
+    "dartRefreshTimeMsec"   : 0,
+    "slideShowAdsRefresh"   : true
+  },
+  "allure"        : {
+    "siteName"              : "allure",
+    "dartRefresh"           : false,
+    "dartRefreshTimeMsec"   : 0,
+    "slideShowAdsRefresh"   : true
+  },
+  "gq"            : {
+    "siteName"              : "gq",
+    "dartRefresh"           : false,
+    "dartRefreshTimeMsec"   : 0,
+    "slideShowAdsRefresh"   : true
+  },
+  "vanityfair"    : {
+    "siteName"              : "vanityfair",
+    "dartRefresh"           : false,
+    "dartRefreshTimeMsec"   : 0,
+    "slideShowAdsRefresh"   : true
+  },
+  "newyorker"    : {
+    "siteName"              : "newyorker",
+    "dartRefresh"           : false,
+    "dartRefreshTimeMsec"   : 0,
+    "slideShowAdsRefresh"   : true
+  }
+}
